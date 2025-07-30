0df5d343afca2dc9f3fe31347cf826018f2a0f83
Oleg Shipulev <oshipulev@gmail.com>
2025-07-30 15:20:24 +0300

    добавлена фикстура

---
+++ conftest.py
@@ -0,0 +1,12 @@
+import pytest
+from selenium import  webdriver
+
+from data import TestData
+
+
+@pytest.fixture
+def driver():
+    driver = webdriver.Firefox()
+    driver.get(TestData.main_url)
+    yield driver
+    driver.quit()
--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -8,20 +8,18 @@ class TestMainPageDropList:
 
     driver = None
 
-    @classmethod
-    def setup_class(cls):
-        cls.driver = webdriver.Firefox()
-        cls.driver.get(TestData.main_url)
+    # @classmethod
+    # def setup_class(cls):
+    #     cls.driver = webdriver.Firefox()
+    #     cls.driver.get(TestData.main_url)
 
     @pytest.mark.parametrize('question_number, expected_answer', TestData.test_data_question_answer)
-    def test_check_text_in_answer(self, question_number, expected_answer):
-        main_page = MainPage(self.driver)
+    def test_click_dropdown_list_answer_text_expected(self, driver, question_number, expected_answer):
+        main_page = MainPage(driver)
         main_page.scroll_to_list()
         main_page.click_on_question(question_number)
         assert main_page.get_text_of_answer(question_number) == expected_answer
 
-    @classmethod
-    def teardown_class(cls):
-        cls.driver.quit()
-
-
+    # @classmethod
+    # def teardown_class(cls):
+    #     cls.driver.quit()
