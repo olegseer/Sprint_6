5607c8db633681f17ff0ad3d96f4972c2ea31d71
Oleg Shipulev <oshipulev@gmail.com>
2025-07-31 11:41:12 +0300

    тест бронирования теперь параметризованный

--- pages/order_page.py
+++ pages/order_page.py
@@ -9,24 +9,24 @@ class OrderPage(BasePage):
     def send_keys_to_input(self, locator, keys):
         self.driver.find_element(*locator).send_keys(keys)
 
-    def filling_first_form(self):
-        self.send_keys_to_input(OrderPageLocators.name, 'Иван')
-        self.send_keys_to_input(OrderPageLocators.last_name, 'Иванов')
-        self.send_keys_to_input(OrderPageLocators.address, 'Москва')
-        self.send_keys_to_input(OrderPageLocators.metro, 'Черкизовская')
+    def filling_first_form(self, test_data):
+        self.send_keys_to_input(OrderPageLocators.name, test_data[0])
+        self.send_keys_to_input(OrderPageLocators.last_name, test_data[1])
+        self.send_keys_to_input(OrderPageLocators.address, test_data[2])
+        self.send_keys_to_input(OrderPageLocators.metro, test_data[3])
         self.click_on_element(OrderPageLocators.station_metro)
-        self.send_keys_to_input(OrderPageLocators.phone, '89998887766')
+        self.send_keys_to_input(OrderPageLocators.phone, test_data[4])
         self.wait_element_stay_clickable(OrderPageLocators.next_btn)
         self.click_on_element(OrderPageLocators.next_btn)
 
-    def filling_second_form(self):
+    def filling_second_form(self, test_data):
         self.wait_visibility_of_element(OrderPageLocators.make_order_btn)
         self.click_on_element(OrderPageLocators.when_deliver)
-        self.send_keys_to_input(OrderPageLocators.when_deliver, '01.08.2025')
+        self.send_keys_to_input(OrderPageLocators.when_deliver, test_data[5])
         self.click_on_element(OrderPageLocators.grey_scooter)
         self.click_on_element(OrderPageLocators.rent_time)
         self.click_on_element(OrderPageLocators.rent_time_option)
-        self.send_keys_to_input(OrderPageLocators.comment, 'комментарий')
+        self.send_keys_to_input(OrderPageLocators.comment, test_data[6])
         self.click_on_element(OrderPageLocators.make_order_btn)
         self.wait_visibility_of_element(OrderPageLocators.yes_btn)
         self.click_on_element(OrderPageLocators.yes_btn)
--- tests/test_order_page.py
+++ tests/test_order_page.py
@@ -1,3 +1,6 @@
+import pytest
+
+from data import TestData
 from locators.main_page_locators import MainPageLocators
 from locators.order_page_locators import OrderPageLocators
 from pages.order_page import OrderPage
@@ -5,15 +8,15 @@ from pages.order_page import OrderPage
 
 class TestOrderPage:
 
-    def test_order_all_fields_success(self, driver):
+    @pytest.mark.parametrize('button, test_data', [[MainPageLocators.order_btn_top, TestData.test_data_user_1],
+                                                   [MainPageLocators.order_btn_bottom, TestData.test_data_user_2]])
+    def test_order_all_fields_success(self, driver, button, test_data):
         order_page = OrderPage(driver)
         order_page.click_cookie_button()
-        order_page.wait_visibility_of_element(MainPageLocators.order_btn_top)
-        order_page.click_on_element(MainPageLocators.order_btn_top)
-        order_page.filling_first_form()
-        order_page.filling_second_form()
+        order_page.scroll_to_element(button)
+        order_page.wait_visibility_of_element(button)
+        order_page.click_on_element(button)
+        order_page.filling_first_form(test_data)
+        order_page.filling_second_form(test_data)
         assert order_page.check_success_order()
-
-
-
 
